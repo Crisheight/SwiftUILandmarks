3ec609cfb73ab1561ace57d52854314e6ee3aa65
Cris <crisheight@gmail.com>
2024-06-16 17:31:06 -0500

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Cris Rosa on 10/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack (alignment: .leading){
+            Text("Turtle Rock")
+                .font(.title)
+            Text("Joshua Tree National Park")
+                .font(.subheadline)
+        }
+            
+    }
+}
+
+#Preview {
+    ContentView()
+}
